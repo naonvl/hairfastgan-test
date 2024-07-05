22c8f458f83a938806e8c6dd4bfb52806232a20e
nao <chiwoo@postech.ac.kr>
2024-07-05 14:49:15 +0700

    update: store the upload

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import Replicate from 'replicate';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import multer from 'multer';
+import cors from 'cors';
+import fs from 'fs';
+import path from 'path';
 
 dotenv.config();
 
@@ -15,32 +18,46 @@ const replicate = new Replicate({
 const model = 'camenduru/hairfastgan:c467265bc832a51093a26edd7d224eef88de0415659a8a2858b4aa20ccf194c1';
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const upload = multer({ dest: 'uploads/' });
 
-app.post('/upload-face-image', async (req, res) => {
-  const face_image = req.body.face_image;
-  const color_image = face_image;
-  const shape_image = "https://d3ss46vukfdtpo.cloudfront.net/static/media/img_thumbnails_hairstyle1.cf36955e.jpg";
-
-  const filePath = `./uploads/${new Date().getTime()}.jpg`;
-  fs.writeFileSync(filePath, face_image);
-  console.log(`Face image saved at ${filePath}`);
-  const input = {
-    face_image: filePath,
-    color_image: color_image,
-    shape_image: shape_image
-  };
-
-  console.log('Using model: %s', model);
-  console.log('With input: %O', input);
-
-  console.log('Running...');
-  const output = await replicate.run(model, { input });
-  console.log('Done!', output);
-  res.send(output);
-});
+// app.post('/upload-face-image', upload.single('face_image'), async (req, res) => {
+//   const filePath = path.join(__dirname, 'uploads', req.file.originalname);
+//   console.log(filePath);
+//   res.send('output');
+// });
 createServer(app).listen(4000, () => {
   console.log('Server started on port 4000');
+});
+
+app.post("/upload_files", upload.single('face_image'), (req, res) => {
+  const originalname = req.file.originalname;
+  const filepath = path.join(path.resolve(), 'uploads/', req.file.filename);
+  const newfilepath = path.join(path.resolve(), 'uploads/', originalname);
+  const face_image = `http://47.237.84.27:4000/upload/${req.file.filename}`;
+  const color_image = req.body.color_image;
+  const shape_image = "https://d3ss46vukfdtpo.cloudfront.net/static/media/img_thumbnails_hairstyle1.cf36955e.jpg";
+
+  fs.rename(filepath, newfilepath, async (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send({ message: 'Failed to rename file' });
+    } else {
+      const input = {
+        face_image: face_image,
+        color_image: color_image,
+        shape_image: shape_image
+      };
+
+      console.log('Using model: %s', model);
+      console.log('With input: %O', input);
+
+      console.log('Running...');
+      const output = await replicate.run(model, { input });
+      console.log('Done!', output);
+      res.send({ message: 'File uploaded successfully' });
+    }
+  });
 });
